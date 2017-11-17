0b1b1e0a11f3a00b8649a0fd8149f465f0b1dc99
Christina Paik <christinapaik@gmail.com>
2017-11-16 19:41:29 -0500

    add jQuery branch and functions

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    var allParagraphs = $( 'p' );
+});
+
+$('p').css({
+    'background-color': 'lightgray',
+    'font-weight': 'bolder',
+    'font-color': 'blue'
+});
