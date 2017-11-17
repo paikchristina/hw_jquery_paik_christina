6cc96d251f751ee25a6bc33ea2cff48d13625f7a
Christina Paik <christinapaik@gmail.com>
2017-11-16 19:30:21 -0500

    add headings branch and ready function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$( document ).ready(function() {
-    var headings = $( 'h2' );
+//HEADINGS BRANCH
+
+$(document).ready(function () {
+    var headings = $('h2');
 });
+
+$('headings').append("I'm Christina Paik, and I'm learning jQuery!");
+
+//END HEADINGS BRANCH
